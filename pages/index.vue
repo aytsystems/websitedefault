cb91a9f04a780f998f4b7b28056f82894abc7dca
aytsystems <contato@ayt.com.br>
2023-09-16 08:08:56 -0300

    v-snotify

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
       </v-card>
       <v-card>
         <v-card-title class="headline">
-         8h02 Welcome to the Vuetify + Nuxt.js template
+         8h08 add snotify Welcome to the Vuetify + Nuxt.js template
         </v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
